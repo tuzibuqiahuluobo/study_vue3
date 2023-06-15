4d1ae43cac0fdb6d37a91cabe90cae81d6e0b39d
糯米木有糍 <99337892+tuzibuqiahuluobo@users.noreply.github.com>
2023-06-15 16:57:54 +0800

    Add files via upload

--- sql/blog-project.sql
+++ sql/blog-project.sql
@@ -1,17 +1,17 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : MySQL8
+ Source Server         : mysql
  Source Server Type    : MySQL
- Source Server Version : 80032
+ Source Server Version : 80011
  Source Host           : localhost:3306
  Source Schema         : blog-project
 
  Target Server Type    : MySQL
- Target Server Version : 80032
+ Target Server Version : 80011
  File Encoding         : 65001
 
- Date: 15/06/2023 12:56:44
+ Date: 15/06/2023 16:57:20
 */
 
 SET NAMES utf8mb4;
@@ -22,19 +22,34 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `db_account`;
 CREATE TABLE `db_account`  (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE INDEX `unique_name`(`username` ASC) USING BTREE,
-  UNIQUE INDEX `unique_email`(`email` ASC) USING BTREE
+  UNIQUE INDEX `unique_name`(`username`) USING BTREE,
+  UNIQUE INDEX `unique_email`(`email`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of db_account
 -- ----------------------------
 INSERT INTO `db_account` VALUES (1, '2672172829@qq.com', 'user', '$2a$10$BdWIxVLBgIsOHDHYCIG4uOBvHe8Po1a0helDdaa36OGOfwVMMXbcO');
+
+-- ----------------------------
+-- Table structure for email_code
+-- ----------------------------
+DROP TABLE IF EXISTS `email_code`;
+CREATE TABLE `email_code`  (
+  `email` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `code` varchar(6) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  INDEX `email_code_email_uindex`(`email`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of email_code
+-- ----------------------------
+INSERT INTO `email_code` VALUES ('2672172829@qq.com', '407230');
 
 -- ----------------------------
 -- Table structure for persistent_logins
@@ -44,7 +59,7 @@ CREATE TABLE `persistent_logins`  (
   `username` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `series` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `token` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `last_used` timestamp NOT NULL,
+  `last_used` timestamp(0) NOT NULL,
   PRIMARY KEY (`series`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
